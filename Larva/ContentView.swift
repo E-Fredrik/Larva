0e9eecd693cd7cfc440a8e69ad092d1958420cca
Elifele Fredrik Imanuel Jelifta <elifele2006@gmail.com>
2026-05-24 19:26:16 +0700

    Initial Commit

---
+++ Larva/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Larva
+//
+//  Created by Elifele Fredrik on 24/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Larva/LarvaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LarvaApp.swift
+//  Larva
+//
+//  Created by Elifele Fredrik on 24/05/26.
+//
+
+import SwiftUI
+
+@main
+struct LarvaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
