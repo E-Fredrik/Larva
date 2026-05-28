9de58fbb59acc2d43082ef876f46b825584a08c3
Elifele Fredrik Imanuel Jelifta <elifele2006@gmail.com>
2026-05-28 16:06:48 +0700

    Larva Watch App Target

---
+++ LarvaWatchApp Watch App/LarvaWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LarvaWatchAppApp.swift
+//  LarvaWatchApp Watch App
+//
+//  Created by Elifele Fredrik on 28/05/26.
+//
+
+import SwiftUI
+
+@main
+struct LarvaWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
