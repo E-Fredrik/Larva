5f1f1becbb38032396b80c46f0199124dcf8e01a
Dave03062006 <146943532+Dave03062006@users.noreply.github.com>
2026-05-28 10:23:09 +0700

    Added ProfileViewModel & Component

---
+++ Larva/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  ProfileViewModel.swift
+//  Larva
+//
+//  Created by student on 28/05/26.
+//
+
+import Foundation
+import Combine
+import SwiftUI
+
+class ProfileViewModel: ObservableObject {
+    @Published var currentUser: User
+    
+    @Published var stepsToday: Int = 5230
+    @Published var distanceToday: Double = 3.2
+    
+    var friendCount: Int{
+        currentUser.friendList.count
+    }
+    
+    @Published var equippedCustomization: [ShopItem] = [
+        ShopItem(id: "theme_midnight", name: "Midnight Mint", description: "Midnight Themed mode map with glowing mint accents", cost: 500, itemType: .mapTheme),
+        ShopItem(id: "border_gold", name: "Golden Frame", description: "A Shiny gold border for your leaderboard avatar", cost: 300, itemType: .avatarBorder)
+    ]
+    init(currentUser: User){
+        self.currentUser = currentUser
+    }
+    
+    func logout(){
+        print("Logging out user \(currentUser.username)...")
+    }
+}
